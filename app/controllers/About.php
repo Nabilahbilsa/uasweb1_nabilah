bedcffc46f49f33f14ffb98fd4de735d3ac4d208
Nabilahbilsa <bnabilah462@gmail.com>
2025-07-14 20:05:13 +0700

    Implementasikan Method Render di Core View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,21 @@
 <?php
 
-class About {
-    public function index()
+class About extends Controller{
+    public function index($nama = 'Nabilah', $pekerjaan = 'Mahasiswa', $umur = 19)
     {
-        echo 'About/index';
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('About/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('About/page');
+        $this->view('templates/footer');
     }
 }
