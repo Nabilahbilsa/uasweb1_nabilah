43b1a18f4de4273c81e68fdea9788aba6cb7108c
Nabilahbilsa <bnabilah462@gmail.com>
2025-07-15 11:22:22 +0700

    Buat ItemModel getAll() dan getByld()

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL','http://localhost/uasweb1_nabilah/public');
+
+//db
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'uasweb1_nabilah'); 
--- app/models/Mahiswa_model.php
+++ app/models/Mahiswa_model.php
@@ -3,43 +3,64 @@
 class Mahasiswa_model{
     private $dbh;
     private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        $dsn = 'mysql:host=localhost;dbname=uasweb1_aditsatriaalamsyah';
+        $dsn = 'mysql:host=localhost;dbname=uasweb1_nabilah';
 
         try {
             $this->dbh = new PDO($dsn,'root', '');
         } catch(PDOException $e) {
             die ($e->getMessage());
         }
+        $this->db = new Database;
     }
 //     private $mhs = [
 //     [
-//         "nama" => "Adit Satria Alamsyah",
-//         "nim" => "23012020",
-//         "email" =>"aditsatriaalamsyah123@gmail.com",
+//         "nama" => "nabilah",
+//         "nim" => "23012012",
+//         "email" =>"bnabilah462@gmail.com",
 //         "jurusan" => "manajemen informatika"
 //     ],
 //     [
-//         "nama" => "ahmad",
-//         "nim" => "23012020",
-//         "email" =>"aditsatriaalamsyah123@gmail.com",
+//         "nama" => "rara",
+//         "nim" => "23012012",
+//         "email" =>"bnabilah462@gmail.com",
 //         "jurusan" => "manajemen informatika"
 //     ],
 //     [
-//         "nama" => "bario",
-//         "nim" => "23012020",
-//         "email" =>"aditsatriaalamsyah123@gmail.com",
+//         "nama" => "nindi",
+//         "nim" => "23012012",
+//         "email" =>"bnabilah462@gmail.com",
 //         "jurusan" => "manajemen informatika"
-//     ]
+//     ],
+//     [
+//       "nama" => "dina",
+//         "nim" => "23012012",
+//         "email" =>"bnabilah462@gmail.com",
+//         "jurusan" => "manajemen informatika"
+//       ]
 // ];
+
+    
+
 
 public function getALLMahasiswa()
 {
     $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
     $this->stmt->execute();
     return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    $this->db->query('SELECT * FROM '. $this->table);
+    return $this->db->resultSet();
 }
 
+public function getMahasiswaById($id)
+{
+$this->db->query("SELECT * FROM mahasiswa WHERE id = :id");
+$this->db->bind('id', $id);
+return $this->db->single();
+
+}
 }
