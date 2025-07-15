a3d690fdcb4f29bd418b0e161546f6d4c015b422
Nabilahbilsa <bnabilah462@gmail.com>
2025-07-15 23:12:23 +0700

    insert: buat modal box dan form input di view

--- app/core/Database.php
+++ app/core/Database.php
@@ -4,59 +4,53 @@ class Database {
     private $host = DB_HOST;
     private $user = DB_USER;
     private $pass = DB_PASS;
-    private $db_name = DB_NAME;
+    private $name = DB_NAME;
 
     private $dbh;
     private $stmt;
 
     public function __construct()
     {
-       
-    
-        $dsn = 'mysql:host=' . $this->host.';dbname='.$this->db_name;
-        
-        $option =[
-            PDO::ATTR_PERSISTENT => True,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+
+        //data source name
+        $dsn = 'mysql:host=' .$this->host . ';dbname=' . $this->db_name;
+
+
+        $option = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRRMODE => PDO::ERRRMODE_EXCEPTION
         ];
 
         try {
-            $this->dbh = new PDO($dsn,$this->user, $this->pass, $option);
+            $this->dbh = new PDO ($dsn, $this->user, $this->pass, $option);
         } catch(PDOException $e) {
-            die ($e->getMessage());
+            die($e->getmessage());
         }
-     
     }
 
     public function query($query)
     {
         $this->stmt = $this->dbh->prepare($query);
     }
-
-    public function bind ($param, $value, $type =null)
+    public function bind($param, $value, $type = null)
     {
-        if(is_null($type)){
-            switch(true){
-                case is_int($value):
-                $type = PDO::PARAM_INT;
-                break;
-                case is_bool($value):
+        if ( is_null($type) ) {
+            switch( tru ) {
+                case is_int($value) :
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value) :
                     $type = PDO::PARAM_BOOL;
                     break;
-                case is_null($value):
+                case is_null($value) :
                     $type = PDO::PARAM_NULL;
                     break;
-                default :
-                $type = PDO::PARAM_STR;
-                 
-
+                    default :
+                    $type = PDO::PARAM_STR;
             }
         }
-
-
-
-        $this->stmt->bindValue($param,$value,$type);
-    }
+        $this->stmt->bindValue($param, $value, $typr);
+}
 
     public function execute()
     {
@@ -67,14 +61,16 @@ class Database {
     {
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+    } 
 
-    public function single()
+     public function single()
     {
         $this->execute();
         return $this->stmt->fetch(PDO::FETCH_ASSOC);
-
     }
 
-
+    public function rowCount()
+    {
+        return $this->stmt->rowCount();
+    }
 }
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -45,22 +45,30 @@ class Mahasiswa_model{
 // ];
 
     
-
-
-public function getALLMahasiswa()
-{
-    $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-    $this->stmt->execute();
-    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-    $this->db->query('SELECT * FROM '. $this->table);
-    return $this->db->resultSet();
-}
-
 public function getMahasiswaById($id)
-{
-$this->db->query("SELECT * FROM mahasiswa WHERE id = :id");
-$this->db->bind('id', $id);
-return $this->db->single();
-
+     {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id'); 
+     }
+     $this->db->bind('id', $id);
+     return $this->db->single();
 }
+
+public function tambah DataMahasiswa($data)
+{
+    $query = "INSERT INTO mahasiswa
+        VALUES
+        ('', :nama, :nim, :email, :jurusan)";
+
+    $this->db->query($query);
+    $this->db->bind('nama', $data['nama']);
+    $this->db->bind('nim', $data['nim']);
+    $this->db->bind('email', $data['email']);
+    $this->db->bind('jurusan', $data['jurusan']);
+
+
+    this->db->execute();
+
+
+    return $this->db->rowCount();
+    
 }
